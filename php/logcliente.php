1a84f50885f2e7e2f874561f014d1e2e24ede477
Gloton <gloton@gmail.com>
2014-01-07 04:03:55 -0500

    Login basico

---
+++ php/logcliente.php
@@ -0,0 +1,16 @@
+<?php 
+$contador = 0;
+$conexion = mysqli_connect('localhost','root','root','tiendaonline');
+mysqli_set_charset($conexion, "utf8");
+$peticion = "SELECT * FROM clientes WHERE usuario = '".$_POST['usuario']."' AND contrasena = '".$_POST['contrasena']."'";
+$resultado = mysqli_query($conexion, $peticion);
+while($fila = mysqli_fetch_array($resultado)) {
+	$contador++;
+}
+if($contador > 0){
+	echo 'El usuario existe';
+} else {
+	echo 'El usuario NO existe';
+}
+mysqli_close($conexion);
+?>
