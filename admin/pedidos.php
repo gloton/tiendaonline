fe7032c856cfc24b6fa66f23a4a3caa06a9045c5
Gloton <gloton@gmail.com>
2014-01-08 05:27:25 -0500

    Ordenar y colorar pedido segun estado del pedido

--- admin/pedidos.php
+++ admin/pedidos.php
@@ -1,11 +1,17 @@
 <table border="1">
 <?php 
+
 $conexion = mysqli_connect('localhost','root','root','tiendaonline');
 mysqli_set_charset($conexion, "utf8");
-$peticion = "SELECT * FROM pedidos";
+$peticion = "SELECT * FROM pedidos ORDER BY estado, fecha ASC";
 $resultado = mysqli_query($conexion, $peticion);
 while($fila = mysqli_fetch_array($resultado)) {
-	echo '<tr><td>'.$fila['idcliente'].'</td><td>'.$fila['fecha'].'</td><td>'.$fila['estado'].'</td><td></td></tr>';
+	$estado = $fila['estado'];
+	 if($estado == 0){$diestado = "no servido";}else{$diestado = "servido";}
+	 
+	 echo '<tr';
+	 if($estado == 0){echo ' style="background:rgb(255,200,200);"';}else{echo ' style="background:rgb(200,255,200);"';}
+	echo '><td>'.$fila['idcliente'].'</td><td>'.date("M d Y H:i:s", $fila['fecha']).'</td><td>'.$diestado.'</td><td></td></tr>';
 }
 mysqli_close($conexion);
 ?>
