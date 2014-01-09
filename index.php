79a2a90bce041cc169c33cefca162cf998643227
Gloton <gloton@gmail.com>
2014-01-09 11:16:20 -0500

    Se le da un poco de estetica al frontend

--- index.php
+++ index.php
@@ -9,13 +9,14 @@ while($fila = mysqli_fetch_array($resultado)) {
 	$peticion2 = "SELECT * FROM imagenesproductos WHERE idproducto = ".$fila['id']." LIMIT 1";
 	$resultado2 = mysqli_query($conexion, $peticion2);
 
+	while($fila2 = mysqli_fetch_array($resultado2)) {
+		echo "<br /><img src='photo/".$fila2['imagen']."' width=100px>";
+	}
+	
 	echo "<a href='producto.php?id=".$fila['id']."'><h3>".$fila['nombre']."</h3></a>";
 	echo "<p>".$fila['descripcion']."</p>";
 	echo "<p>Precio: ".$fila['precio']." €</p>";
 	
-	while($fila2 = mysqli_fetch_array($resultado2)) {
-		echo "<br /><img src='photo/".$fila2['imagen']."' width=100px>";
-	}
 
 	echo "<br>";
 	echo "<a href='producto.php?id=".$fila['id']."'><button>Más información</button></a>";
